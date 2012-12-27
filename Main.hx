37c85093f858a5f9c9ae64001d8c78566bb3c349
Simon Krajewski <simon.krajewski@simn.de>
2012-12-27 17:35:18 +0100

    print more characters

--- Main.hx
+++ Main.hx
@@ -13,10 +13,35 @@ class Main extends flash.display.Sprite
 		var tf = new flash.text.TextField();
 		tf.embedFonts = true;
 		tf.defaultTextFormat = fmt;
-		tf.text = "Hello World\nSome more blah";
-		tf.x = tf.y = 50;
-		tf.autoSize = flash.text.TextFieldAutoSize.LEFT;
-		//tf.border = true;
+		tf.wordWrap = true;
+		tf.multiline = true;
+		tf.width = flash.Lib.current.stage.stageWidth;
+		tf.height = flash.Lib.current.stage.stageHeight;
+		var all = [];
+		var nlAfter = new IntHash();
+		nlAfter.set("9".code, true);
+		nlAfter.set("Z".code, true);
+		nlAfter.set("z".code, true);
+		for (i in 33...0xFF)
+			all.push(i);
+			
+		var buf = new StringBuf();
+		function add(i) {
+			buf.addChar(i);
+			all.remove(i);
+			if (nlAfter.exists(i)) {
+				buf.addChar(10);
+			}
+		}
+		for (i in '0'.code...'9'.code + 1) 
+			add(i);			
+		for (i in 'a'.code...'z'.code + 1) 
+			add(i);
+		for (i in 'A'.code...'Z'.code + 1) 
+			add(i);
+		for (i in all)
+			add(i);
+		tf.text = buf.toString();
 		addChild(tf);
 	}
 
